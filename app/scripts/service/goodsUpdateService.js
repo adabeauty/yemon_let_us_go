fdd2eb2ff8e5c296fe6a50834f01c5d472f03251
adabeauty <654296217@qq.com>
2014-09-11 23:08:35 +0800

    modify goodsUpdateService name update

--- app/scripts/service/goodsUpdateService.js
+++ app/scripts/service/goodsUpdateService.js
@@ -1,12 +1,12 @@
 angular.module('letGoApp').service('goodsUpdateService', function(localStorageService){
 
     this.updateItem = function(){
-
+      
         var updateObject = localStorageService.get('updateItem');
         var allGoods = localStorageService.get('allGoods');
         var index = _.findIndex(allGoods, {'name': updateObject.name});
         allGoods[index] = updateObject;
-        console.log(updateObject);
+
         localStorageService.set('allGoods', allGoods);
         return index;
     };
