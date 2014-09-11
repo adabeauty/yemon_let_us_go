569a134d0c483919d573a2db271d7c31671106f1
adabeauty <654296217@qq.com>
2014-09-11 14:30:14 +0800

    add getAllCategories test to categoryAddService test

---
+++ test/spec/service/goodsAddService.js
@@ -0,0 +1,155 @@
+ddescribe('test', function(){
+
+    beforeEach(module('letGoApp'));
+
+    var store = {};
+    var goodsAddService, $location, localStorageService;
+    beforeEach(inject(function($injector){
+
+        goodsAddService = $injector.get('goodsAddService');
+        localStorageService = $injector.get('localStorageService');
+        $location = $injector.get('$location');
+
+        spyOn(localStorageService,'get').andCallFake(function (key) {
+            return store[key];
+        });
+        spyOn(localStorageService,'set').andCallFake(function (key,value) {
+            return store[key] = value;
+        });
+    }));
+
+    describe('test category:', function(){
+        var good;
+        beforeEach(function(){
+            good = {category:'饮料类',  name:'雪碧', price:'3.00', unit:'瓶'};
+        });
+        it('category is ok', function(){
+            var  item = goodsAddService.item(good.category, good.name, good.price, good.unit);
+
+            expect(item.category).toEqual('饮料类');
+            expect(item.name).toEqual('雪碧');
+            expect(item.price).toEqual('3.00');
+            expect(item.unit).toEqual('瓶');
+        });
+    });
+
+    describe('test itemHasExist:', function(){
+        beforeEach(function(){
+              good = [{category:'饮料类',  name:'雪碧', price:'3.00', unit:'瓶'}];
+              localStorageService.set('allGoods', good);
+        });
+        it('itemHasExist is true', function(){
+            var itemHasExist = goodsAddService.itemHasExist('雪碧');
+            expect(itemHasExist).toEqual(0);
+        });
+        it('itemHasExist is false', function(){
+            var itemHasExist = goodsAddService.itemHasExist('可乐');
+            expect(itemHasExist).toEqual(-1);
+        });
+    });
+
+    describe('test itemDetailSuccess:', function(){
+
+      it('itemDetailSuccess is ok', function(){
+
+          var result_0 = goodsAddService.itemDetailSuccess(false, false, false, true);
+          var result_1 = goodsAddService.itemDetailSuccess(true, true, true, true);
+
+          expect(result_0).toEqual(false);
+          expect(result_1).toEqual(true);
+      });
+    });
+
+    describe('test saveItem:', function(){
+        var allGoods;
+        beforeEach(function(){
+
+            allGoods = [  {category:'饮料类',  name:'可乐', price:'3.00', unit:'瓶'},
+                          {category:'饮料类',  name:'橙汁', price:'3.00', unit:'瓶'}
+                        ];
+            var item = {category:'饮料类',  name:'雪碧', price:'3.00', unit:'瓶'};
+            spyOn(goodsAddService, 'item').andReturn(item);
+        });
+        it('allGoods is null', function(){
+
+            localStorageService.set('allGoods', '');
+
+            goodsAddService.saveItem('饮料类', '雪碧', '3.00', '瓶');
+            var currentItems = localStorageService.get('allGoods');
+
+            expect(goodsAddService.item).toHaveBeenCalled();
+            expect(currentItems.length).toBe(1);
+        });
+        it('allGoods isnot null', function(){
+            localStorageService.set('allGoods', allGoods);
+
+            goodsAddService.saveItem('饮料类', '雪碧', '3.00', '瓶');
+            var currentItems = localStorageService.get('allGoods');
+
+            expect(goodsAddService.item).toHaveBeenCalled();
+            expect(currentItems.length).toBe(3);
+        });
+    });
+
+    describe('test addCategoryNum', function(){
+      var category;
+        beforeEach(function(){
+            category = [{ID:'TF1001', name:'饮料类', num: 3}];
+            localStorageService.set('category', category);
+
+            // item = {category:'饮料类',  name:'可乐', price:'3.00', unit:'瓶'};
+        });
+        it('addCategoryNum is ok', function(){
+            goodsAddService.addCategoryNum('饮料类');
+            var result = localStorageService.get('category');
+
+            expect(result[0].num).toEqual(4);
+        });
+    });
+
+    describe('test saveButton:', function(){
+        it('itemDetail isnot integreted', function(){
+            spyOn(goodsAddService, 'itemDetailSuccess').andReturn(false);
+            spyOn(goodsAddService, 'itemHasExist').andReturn(-1);
+
+            var result = goodsAddService.saveButton('饮料类', '雪碧', '3.00', '瓶');
+            expect(result).toEqual(false);
+        });
+        it('only itemName has existed', function(){
+            spyOn(goodsAddService, 'itemDetailSuccess').andReturn(true);
+            spyOn(goodsAddService, 'itemHasExist').andReturn(1);
+
+            var result = goodsAddService.saveButton('饮料类', '雪碧', '3.00', '瓶');
+            expect(result).toEqual(false);
+        });
+        it('saveButton is ok', function(){
+            spyOn(goodsAddService, 'itemDetailSuccess').andReturn(true);
+            spyOn(goodsAddService, 'itemHasExist').andReturn(-1);
+            spyOn(goodsAddService, 'saveItem');
+            spyOn(goodsAddService, 'addCategoryNum');
+            spyOn($location, 'path');
+
+            var result = goodsAddService.saveButton({name:'饮料类'}, '雪碧', '3.00', '瓶');
+
+            expect(goodsAddService.saveItem).toHaveBeenCalledWith('饮料类', '雪碧', '3.00', '瓶');
+            expect(goodsAddService.addCategoryNum).toHaveBeenCalledWith('饮料类');
+            expect($location.path).toHaveBeenCalledWith('/goodsManage');
+            expect(result).toEqual(true);
+        });
+    });
+
+    describe('test getAllCategories', function(){
+        beforeEach(function(){
+            var category = [  {category:'饮料类',  name:'可乐', price:'3.00', unit:'瓶'},
+                              {category:'饮料类',  name:'橙汁', price:'3.00', unit:'瓶'}
+                            ];
+            localStorageService.set('category', category);
+        });
+        it('getAllCategories is ok', function(){
+
+            var result = goodsAddService.getAllCategories();
+            expect(result.length).toEqual(2);
+            expect(result[0].name).toEqual('可乐');
+        });
+    });
+});
